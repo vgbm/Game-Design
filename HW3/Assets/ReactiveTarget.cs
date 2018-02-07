6e32f920e6984c841d8b4871328c5aeed335763d
James Vaughn <james.thomas.vaughn@gmail.com>
2018-02-07 00:31:30 -0500

    Starting hw3

---
+++ HW3/Assets/ReactiveTarget.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	public bool isDead = false;
+
+	public void ReactToHit() {
+		WanderingAI behavior = GetComponent<WanderingAI>();
+		if (behavior != null) {
+			behavior.SetAlive(false);
+			isDead = true;
+		}
+	}
+
+	public void Update() {
+		if (isDead) {
+			this.transform.Rotate (-1, 0, 0);
+
+			if (this.transform.rotation.eulerAngles.x == 90 || this.transform.rotation.eulerAngles.x == 270) {
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
---
+++ HW3/Assets/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneController : MonoBehaviour {
+	[SerializeField] private GameObject enemyPrefab;
+	private List<GameObject> _enemies = new List<GameObject>();
+	private int enemyCount = 1;
+
+	void Start() {
+		_enemies.Add(createNewEnemy());
+	}
+
+	void Update() {
+		//remove the dead and gone enemies from the scene's list
+		_enemies.RemoveAll (enemy => enemy == null);
+
+		//add any new enemies to the scene
+		if (_enemies.Count < enemyCount) {
+			_enemies.Add(createNewEnemy());
+			_enemies.Add(createNewEnemy());
+			enemyCount += 1;
+
+		}
+	}
+
+	private GameObject createNewEnemy() {
+		var newEnemy = Instantiate (enemyPrefab) as GameObject;
+		newEnemy.transform.position = new Vector3(0, 1, 0);
+		float angle = Random.Range(0, 360);
+		newEnemy.transform.Rotate(0, angle, 0);
+
+		return newEnemy;
+	}
+}
