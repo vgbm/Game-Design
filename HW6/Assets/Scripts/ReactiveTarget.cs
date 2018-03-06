54fe113124bfeea70859645380354a1679c6cbc2
James Vaughn <james.thomas.vaughn@gmail.com>
2018-03-06 16:52:03 -0500

    Working on HW 6

---
+++ HW6/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	Animator anim;
+
+	void Start(){
+
+		anim = GetComponent<Animator>();
+	}
+
+
+	public void ReactToHit() {
+		WanderingAI behavior = GetComponent<WanderingAI>();
+		if (behavior != null) {
+			behavior.SetAlive(false);
+		}
+		StartCoroutine(Die());
+	}
+
+	private IEnumerator Die() {
+		this.transform.Rotate(-75, 0, 0);
+		anim.SetInteger("zombieToState", 2);
+		
+		yield return new WaitForSeconds(1.5f);
+		
+		Destroy(this.gameObject);
+	}
+}
---
+++ HW6/Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private int enemiesToSpawn;
+	//private GameObject _enemy;
+
+	void Start() {
+		// spawn n zombies
+		for (int i = 0; i < enemiesToSpawn; i++) {
+			SpawnEnemy ();
+		}
+	}
+
+	void Update() {
+		
+	}
+
+	private GameObject SpawnEnemy() {
+		var enemy = Instantiate (enemyPrefab) as GameObject;
+		enemy.transform.position = new Vector3 (Random.Range(-10, 10), 0, Random.Range(-10, 10));
+		float angle = Random.Range (0, 360);
+		enemy.transform.Rotate (0, angle, 0);
+		enemy.GetComponent<WanderingAI> ().SetAlive (true);
+
+		return enemy;
+	}
+}
